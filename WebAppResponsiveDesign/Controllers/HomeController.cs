5ab27d0ce6c468e267c7cec6567f36c89a57eaff
Alex <chess2ryme@gmail.com>
2017-09-20 18:53:14 +1000

    Initial check-in of module WebAppResponsiveDesign

---
+++ WebAppResponsiveDesign/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppResponsiveDesign.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+		}
+
+		public IActionResult JavaScript()
+		{
+			return View();
+		}
+
+		public IActionResult jQuery()
+		{
+			return View();
+		}
+
+		public IActionResult googleMap()
+		{
+			return View();
+		}
+    }
+}
---
+++ WebAppResponsiveDesign/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿// Write your Javascript code.
+
+function changeHtmlContent() {
+    var content =
+        "<h2>What is HTML?</h2>" +
+        "<p>HTML is a markup language for describing web documents.</p>" +
+        "<ul>" +
+            "<li>HTML stands for Hyper Text Markup Language</li>" +
+            "<li>A markup language is a set of markup tags</li>" +
+            "<li>HTML documents are described by HTML tags</li>" +
+            "<li>Each HTML tag describes different document content</li>" +
+        "</ul>" +
+        "<p>www.w3schools.com</p>";
+
+    document.getElementById("mydiv").innerHTML = content;
+}
+
+// document.getElementById("mydiv").style = "color:blue; background-color:lightgrey;";
+
+// document.getElementById("mydiv").className = "btn-primary";
+
+function makeTextLarger(obj) {
+    obj.style.fontSize = "30px";
+}
+
+function makeTextSmaller(obj) {
+    obj.style.fontSize = "10px";
+}
