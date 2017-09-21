08a5787a6a7acfe4479c9d0ecfe46025a0ccdf4b
Alex <chess2ryme@gmail.com>
2017-09-21 15:17:41 +1000

    Commit after finalising most pages

--- WebAppResponsiveDesign/Controllers/HomeController.cs
+++ WebAppResponsiveDesign/Controllers/HomeController.cs
@@ -7,42 +7,43 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebAppResponsiveDesign.Controllers
 {
     public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
-		}
-
-		public IActionResult JavaScript()
+	{
+		public IActionResult Index()
 		{
 			return View();
 		}
 
-		public IActionResult jQuery()
+		public IActionResult Error()
 		{
 			return View();
 		}
 
-		public IActionResult googleMap()
+        public IActionResult Restaurants()
+        {
+            return View();
+        }
+
+        public IActionResult Cuisine()
+        {
+            return View();
+        }
+
+        public IActionResult Dishes()
+        {
+            return View();
+        }
+
+		public IActionResult Prices()
+		{
+			return View();
+		}
+
+		public IActionResult Location()
+		{
+			return View();
+		}
+
+		public IActionResult Reviews()
 		{
 			return View();
 		}
